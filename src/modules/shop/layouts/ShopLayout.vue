4b4047b13333da6a893383d984ac2c0c8f2b544e
devDiego06 <diegoxd0604@gmail.com>
2025-04-07 23:52:35 -0500

    avanzado

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <RouterView />
+
+  <!-- Product List -->
+  <HomeView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+import HomeView from '../views/HomeView.vue';
+</script>
