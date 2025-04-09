5d96bba569e473fc0d936256a4b6efdf281d86f4
devDiego06 <diegoxd0604@gmail.com>
2025-04-09 15:15:41 -0500

    newCommit

--- src/modules/products/actions/get-product-image.action.ts
+++ src/modules/products/actions/get-product-image.action.ts
@@ -1,5 +1,5 @@
 export const getProductImageActions = (imageName: string): string => {
   return imageName.includes('http')
     ? imageName
-    : `${import.meta.env.VITE_TESLO_API_URL}/products/${imageName}`;
+    : `${import.meta.env.VITE_TESLO_API_URL}/files/product/${imageName}`;
 };
---
+++ src/modules/shop/components/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Tab Menu -->
+
+  <div
+    class="mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+  >
+    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '@/modules/products/actions/interfaces/product.interface';
+import ProductCard from './ProductCard.vue';
+
+interface Props {
+  products: Product[];
+}
+
+defineProps<Props>();
+</script>
